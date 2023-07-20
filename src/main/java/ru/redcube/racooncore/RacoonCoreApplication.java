4bf6216dc9568aa821af3f58e5f9731db4d880cd
Павел Ефремов <test_ac_1@mail.ru>
2023-07-20 11:47:49 +0500

    initial commit

---
+++ src/main/java/ru/redcube/racooncore/RacoonCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.redcube.racooncore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacoonCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RacoonCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/redcube/racooncore/RacoonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.redcube.racooncore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RacoonCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
